8bca589de3cd140a21ceafce7eb94710cc769cb4
Fadlian17 <alfansyahfadlian@gmail.com>
2020-06-04 11:45:15 +0700

    add middleware and post data

--- server/app.js
+++ server/app.js
@@ -2,26 +2,28 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const PORT = 5000
-const {MONGOURI} = require('./keys')
+const { MONGOURI } = require('./keys')
 
 require('./models/user')
+require('./models/post')
 
 app.use(express.json())
 app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
 
 //koneksi pada mongoDB Atlas
-mongoose.connect(MONGOURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
-mongoose.connection.on('connected',()=>{
-    console.log("connected to mongoDB")
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongoDB')
 })
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
+mongoose.connection.on('error', err => {
+  console.log('err connecting', err)
 })
 
-app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
+app.listen(PORT, () => {
+  console.log('server is running on', PORT)
 })
--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,6 @@
-module.exports={
-//koneksi pada mongoDB Atlas
-    MONGOURI:"mongodb+srv://Alfansyah:dZKFOlmk7CSvS4mQ@cluster0-vpxic.mongodb.net/test?retryWrites=true&w=majority"
-}
+module.exports = {
+  //koneksi pada mongoDB Atlas
+  MONGOURI:
+    "mongodb+srv://Alfansyah:dZKFOlmk7CSvS4mQ@cluster0-vpxic.mongodb.net/test?retryWrites=true&w=majority",
+  JWT_SECRET: "efjajsd812",
+};
